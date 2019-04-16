2fe286cfe87ced3a0a1469df9cd203ce98b3f615
srikanthpragada <srikanthpragada@gmail.com>
2019-04-16 19:58:50 +0530

    16-APR

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} - {self.title} - {self.author} - {self.price}"
